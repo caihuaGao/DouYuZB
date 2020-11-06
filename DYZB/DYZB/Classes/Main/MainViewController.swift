84f0e7db9ff43733ce308c08288b8c7fa42afd88
gaocaihua <gaocaihua@worktrans.cn>
2020-11-06 18:05:20 +0800

    添加图片 设置StoryBoard

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by lx on 2020/11/6.
+//  Copyright © 2020 xingyuxinghua. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+    }
+    
+
+
+
+}
